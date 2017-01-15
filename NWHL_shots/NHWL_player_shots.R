70f158df9dbc173111943177a35908384b989cdf
conorotompkins <conorotompkins@gmail.com>
2017-01-15 18:05:30 -0500

    created new file in correct folder

--- NWHL_shots/NHWL_player_shots.R
+++ NWHL_shots/NHWL_player_shots.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+setwd("C:/Users/conor/githubfolder/Random-Hockey-Charts/NWHL_shots")
 #http://www.nwhl.zone/stats/league_instance/46941?order_by=hksgm&order_dir=desc&stat_tab=ice_hockey_skater&subseason=327125
 
 page_1 <- read.delim("clipboard")
@@ -9,4 +10,10 @@ my_files <- list(page_1 = page_1,
                  page_2 = page_2,
                  page_3 = page_3)
 
-my_data <- bind_rows(my_files)
+
+my_data <- bind_rows(my_files) %>%
+  rename(name = `NAME`,
+         team = `TEAM`) %>%
+  select(team, name, SOG)
+
+write_csv(my_data, "NWHL_shots_data.csv")
