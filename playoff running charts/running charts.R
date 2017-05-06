5a3800a19dd50a3db39105b5d18765a94464566e
conorotompkins <conorotompkins@gmail.com>
2017-05-06 12:00:49 -0400

    updated code

--- playoff running charts/running charts.R
+++ playoff running charts/running charts.R
@@ -11,7 +11,17 @@ df_raw <- read_csv("16_17 playoffs running charts 4_28.csv")
 
 colnames(df_raw) <- tolower(colnames(df_raw))
 
+teams <- df_raw %>%
+  group_by(team) %>% 
+  mutate(team_toi_sd = sd(toi)) %>% 
+  arrange(team_toi_sd) %>% 
+  select(team) %>% 
+  unique() %>% 
+  unlist()
+
+
 df_player <- df_raw %>% 
+  mutate(team = factor(team, levels = teams)) %>% 
   arrange(team, player, date) %>% 
   group_by(team) %>% 
   mutate(game_number = dense_rank(date)) %>% 
@@ -20,21 +30,33 @@ df_player <- df_raw %>%
   mutate(toi_cum = cumsum(toi),
          position = if_else(position == "L" | position == "C" | position ==  "R",
                        "F", "D")) %>% 
-  select(team, player, game_number, position, toi_cum) %>% 
+  select(team, player, game_number, position, toi, toi_cum) %>% 
   group_by(player, team) %>% 
   mutate(toi_sum = max(toi_cum)) %>% 
-  ungroup()
+  ungroup() %>% 
+  group_by(team) %>% 
+  mutate(toi_team_sum = sum(toi)) %>% 
+  ungroup() %>% 
+  mutate(toi_pct_team = round(toi_sum / toi_team_sum, digits = 3))
 
 
+plot_player_league <- df_player %>% 
+  ggplot(aes(game_number, toi_cum, color = position, group = player)) +
+  geom_vline(xintercept = c(1:7), alpha = .25) +
+  geom_line()
+
+plot_player_league
+
+  
 plot_player <- df_player %>% 
   #filter(team == "PIT") %>% 
-    ggplot(aes(game_number, toi_cum, group = player, color = position, size = toi_sum, alpha = toi_sum)) +
+    ggplot(aes(game_number, toi_cum, group = player, color = position, size = toi_pct_team, alpha = toi_pct_team)) +
       geom_vline(xintercept = c(1:7), alpha = .25) +
       geom_line() +
       facet_wrap(~team) +
   scale_color_discrete(guide_colorbar(title = "Player Position")) +
       scale_size_continuous(range = c(.25, 1.25), guide = "none") +
-      scale_alpha_continuous(range = c(.25, 1), guide = "none") +
+      scale_alpha_continuous(range = c(.05, 1), guide = "none") +
       labs(title = "Cumulative Time on Ice Per Player",
        subtitle = "2016-17 NHL Playoffs, All Situations",
        x = "Game Number",
@@ -45,7 +67,8 @@ plot_player <- df_player %>%
 plot_player
 
 df_pos <- df_raw %>% 
-  mutate(position = if_else(position == "L" | position == "C" | position ==  "R",
+  mutate(team = factor(team, levels = teams),
+         position = if_else(position == "L" | position == "C" | position ==  "R",
                             "F", "D")) %>% 
   arrange(team, position, date) %>% 
   group_by(team) %>% 
