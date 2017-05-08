d4ffb5dd2307ecfba303d992162402507336b179
Conor Tompkins <conorotompkins@gmail.com>
2017-05-07 22:20:37 -0400

    added scatter plots and boxplots, updated data

--- playoff running charts/TOI standard deviation graphs.R
+++ playoff running charts/TOI standard deviation graphs.R
@@ -5,7 +5,7 @@ setwd("C:/Users/conor/githubfolder/Random-Hockey-Charts/playoff running charts/"
 
 theme_set(theme_bw())
 
-df_raw <- read_csv("playoff running charts/16_17 playoffs running charts 5_6.csv")
+df_raw <- read_csv("playoff running charts/16_17 playoffs running charts 5_7.csv")
 
 colnames(df_raw) <- tolower(colnames(df_raw))
 
@@ -55,6 +55,16 @@ line_plot_position <- df_pos %>%
         plot.caption = element_text(hjust = 0))
 line_plot_position
 
+boxplot_position <- df_raw %>%
+  mutate(team = factor(team, levels = teams),
+         position = if_else(position == "L" | position == "C" | position ==  "R",
+                            "F", "D"))
+  
+team_position_boxplot <- boxplot_position %>% 
+  ggplot(aes(team, toi, fill = position)) +
+  geom_boxplot()
+team_position_boxplot
+
 team_position <- df_raw %>% 
   mutate(team = factor(team, levels = teams),
          position = if_else(position == "L" | position == "C" | position ==  "R",
@@ -82,8 +92,18 @@ team_bar_position_plot
 team_scatter_position <- team_position %>% 
   select(team, position, toi_sd) %>% 
   spread(key = position,
-         value = toi_sd) %>% 
-  ggplot(aes(D, F, label = team)) +
-  geom_label()
+         value = toi_sd,
+         sep = "_TOI_SD_") %>% 
+  ggplot(aes(position_TOI_SD_D, position_TOI_SD_F, label = team)) +
+  geom_label() +
+  annotate(geom = "text",
+           x = 6,
+           y = 4.4,
+           label = "Higher --> More Variance") +
+  labs(x = "Standard Deviation of Defense TOI",
+       y = "Standard Deviation of Forwards TOI",
+       title = "Distribution of Forward and Defense Lines",
+       subtitle = "2016-17 NHL Playoffs, All-Situations Play",
+       caption = "@Null_HHockey, data from Corsica.Hockey.CA")
 team_scatter_position
 ?spread
