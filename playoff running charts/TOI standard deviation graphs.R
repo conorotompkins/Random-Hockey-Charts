0944a7af3ad8a5232d764348e6a08fd18bc9dde4
Conor Tompkins <conorotompkins@gmail.com>
2017-05-12 22:04:25 -0400

    merged changes from "running charts" script

--- playoff running charts/TOI standard deviation graphs.R
+++ playoff running charts/TOI standard deviation graphs.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 library(viridis)
 
-setwd("C:/Users/conor/githubfolder/Random-Hockey-Charts/playoff running charts/")
+setwd("~/github folder/Random-Hockey-Charts/playoff running charts")
 
 theme_set(theme_bw())
 
-df_raw <- read_csv("16_17 playoffs running charts 5_7.csv")
+df_raw <- read_csv("16_17 playoffs running charts 5_12.csv")
 
 colnames(df_raw) <- tolower(colnames(df_raw))
 
@@ -17,6 +17,24 @@ teams <- df_raw %>%
   unique() %>% 
   unlist()
 
+teams_pos <- df_raw %>%
+  arrange(team, position, date) %>% 
+  group_by(team) %>% 
+  mutate(game_number = dense_rank(date),
+         team_toi_sd = sd(toi),
+         gp = max(game_number)) %>% 
+  arrange(desc(gp), team_toi_sd) %>% 
+  select(team) %>% 
+  unique() %>% 
+  unlist()
+
+
+league_sd <- df_raw %>% 
+  mutate(position = if_else(position == "L" | position == "C" | position ==  "R",
+                            "F", "D")) %>% 
+  group_by(position) %>% 
+  summarize(league_toi_sd = sd(toi))
+
 eliminated <- list(team = c("BOS",
                             "CHI",
                             "MIN",
@@ -27,7 +45,7 @@ eliminated <- list(team = c("BOS",
                             "TOR"))
 
 df_pos <- df_raw %>% 
-  mutate(team = factor(team, levels = teams),
+  mutate(team = factor(team, levels = teams_pos),
          position = if_else(position == "L" | position == "C" | position ==  "R",
                             "F", "D")) %>% 
   arrange(team, position, date) %>% 
@@ -38,10 +56,11 @@ df_pos <- df_raw %>%
   group_by(team, game_number, position) %>% 
   summarize(toi_sd = sd(toi)) %>% 
   mutate(eliminated = team %in% eliminated$team) %>% 
-  arrange(desc(eliminated))
+  left_join(league_sd, by = c("position"))
 
 line_plot_position <- df_pos %>% 
   ggplot(aes(game_number, toi_sd, color = position)) +
+  geom_hline(aes(color = position, yintercept = league_toi_sd)) +
   geom_vline(xintercept = c(1:max(df_pos$game_number)), alpha = .25) +
   geom_line(size = 2) +
   facet_wrap(~team) +
@@ -49,7 +68,7 @@ line_plot_position <- df_pos %>%
        subtitle = "2016-17 NHL Playoffs, All Situations",
        x = "Game Number",
        y = "Standard Deviation of Time on Ice",
-       caption = "A higher standard deviation indicates greater variance in time on ice @Null_HHockey, data from http://www.corsica.hockey/") +
+       caption = "@Null_HHockey, data from http://www.corsica.hockey/") +
   theme(panel.grid.minor = element_blank(),
         panel.grid.major.x = element_blank(),
         plot.caption = element_text(hjust = 0))
@@ -58,10 +77,8 @@ line_plot_position
 
 
 teams_boxplot <- df_raw %>%
-  group_by(team, date) %>% 
-  summarize(team_toi_sd = sd(toi)) %>% 
   group_by(team) %>% 
-  summarize(team_toi_sd = mean(team_toi_sd)) %>% 
+  summarize(team_toi_sd = sd(toi)) %>% 
   arrange(team_toi_sd) %>% 
   select(team) %>% 
   unique() %>% 
@@ -125,4 +142,6 @@ team_scatter_position <- team_position %>%
        subtitle = "2016-17 NHL Playoffs, All-Situations Play",
        caption = "@Null_HHockey, data from Corsica.Hockey.CA")
 team_scatter_position
-?spread
+
+team_pos_date_df <- df_raw %>% 
+  group_by(team, )
