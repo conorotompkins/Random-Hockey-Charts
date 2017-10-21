6eed7d08e6e03a74538fc1c23a9894af5e83defe
Conor Tompkins <conorotompkins@gmail.com>
2017-10-21 12:39:00 +0000

    ran script again

--- running_team_xg/scripts/running_team_xg.R
+++ running_team_xg/scripts/running_team_xg.R
@@ -4,7 +4,7 @@ library(gghighlight)
 
 theme_set(theme_bw())
 
-data <- read_csv("running_team_xg/data/team_stats_2017-10-16.csv")
+data <- read_csv("running_team_xg/data/team_stats_2017-10-21.csv")
 
 df <- data %>%
   mutate(team = Team,
@@ -18,11 +18,11 @@ df <- data %>%
          xg_pm_cum = cumsum(xg_pm))
 
 df %>% 
-  gghighlight_line(aes(game_number, xg_pm_cum, group = team), last(abs(xg_pm_cum)) > 2) +
+  gghighlight_line(aes(game_number, xg_pm_cum, group = team), last(abs(xg_pm_cum)) > 4) +
   geom_hline(yintercept = 0,
              size = .25,
              linetype = 2) +
-  scale_x_continuous(expand = c(0,0)) +
+  scale_x_continuous(expand = c(0.01,0)) +
   labs(x = "Game Number",
        y = "Cumulative xG Differential",
        caption = "@Null_HHockey, data from corsica.ca",
