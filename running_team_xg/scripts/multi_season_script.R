2beb728c34f60687b059579beecbae0b10f21509
Conor Tompkins <conorotompkins@gmail.com>
2017-10-28 21:55:52 +0000

    updated files, worked on script

--- running_team_xg/scripts/multi_season_script.R
+++ running_team_xg/scripts/multi_season_script.R
@@ -21,39 +21,50 @@ df <- df %>%
   arrange(team, date) %>% 
   group_by(season, team) %>% 
   mutate(game_number = dense_rank(date),
-         xg_pm_cum = cumsum(xg_pm))
+         xg_pm_cum = cumsum(xg_pm)) %>% 
+  ungroup() %>% 
+  mutate(is_current_season = (season == "2017_2018"))
 
 df %>% 
-  gghighlight_line(aes(game_number, xg_pm_cum, group = team), last(abs(xg_pm_cum)) > 20) +
+  #ungroup() %>% 
+  filter(season == "2017_2018") %>% 
+  gghighlight_line(aes(game_number, xg_pm_cum, group = team), last(abs(xg_pm_cum)) > 4) +
   geom_hline(yintercept = 0,
              size = .25,
              linetype = 2) +
-  scale_x_continuous(expand = c(0.01,0)) +
+  scale_x_continuous(expand = c(0.01,0),
+                     breaks = 1:max(df$game_number)) +
   facet_wrap(~season,
              ncol = 1) +
   labs(x = "Game Number",
        y = "Cumulative xG Differential",
        caption = "@Null_HHockey, data from corsica.ca",
        title = "NHL",
-       subtitle = "Even Strength")
+       subtitle = "Even Strength") +
+  theme(panel.grid.minor = element_blank())
 
 df %>% 
-  gghighlight_line(aes(game_number, xg_pm_cum, group = team), unique(team) == "MTL") +
-  #scale_x_continuous(expand = c(0,0)) +
+  filter(season == "2017_2018") %>% 
+  gghighlight_line(aes(game_number, xg_pm_cum, group = team), unique(team) == "VGK") +
+  geom_hline(yintercept = 0,
+             size = .25,
+             linetype = 2) +
+  scale_x_continuous(expand = c(0.01,0),
+                     breaks = 1:max(df$game_number)) +
   facet_wrap(~season,
              ncol = 1) +
   labs(x = "Game Number",
        y = "Cumulative xG Differential")
 
 df %>% 
-  ungroup() %>% 
-  filter(team %in% c("PIT", "WAS", "CBJ", "CAR", "NYR", "NYI")) %>% 
-  ggplot(aes(game_number, xg_pm_cum, color = season)) +
+  filter(team %in% c("PIT", "WSH", "CBJ", "CAR", "NYR", "NYI", "N.J", "PHI")) %>% 
+  ggplot(aes(game_number, xg_pm_cum, color = season, alpha = is_current_season)) +
   geom_hline(yintercept = 0, 
              linetype = 2) +
-  geom_line() +
-  geom_point() +
+  geom_line(size = 1) +
+  #geom_point() +
   facet_wrap(~team) +
+  scale_alpha_discrete(range = c(.25, 1)) +
   labs(x = "Game Number",
        y = "Cumulative xG Differential",
        caption = "@Null_HHockey, data from corsica.ca",
