d8c3cff56d9024499344f8968971a8b0176d024e
Conor Tompkins <conorotompkins@gmail.com>
2017-10-29 13:59:03 +0000

    updated script

--- running_team_xg/scripts/running_team_xg.R
+++ running_team_xg/scripts/running_team_xg.R
@@ -4,7 +4,7 @@ library(gghighlight)
 
 theme_set(theme_bw())
 
-data <- read_csv("running_team_xg/data/team_stats_2017-10-21.csv")
+data <- read_csv("running_team_xg/data/season_2017_2018.csv")
 
 df <- data %>%
   mutate(team = Team,
@@ -22,17 +22,19 @@ df %>%
   geom_hline(yintercept = 0,
              size = .25,
              linetype = 2) +
-  scale_x_continuous(expand = c(0.01,0)) +
+  scale_x_continuous(expand = c(0.01,0),
+                     breaks = 1:max(df$game_number)) +
   labs(x = "Game Number",
        y = "Cumulative xG Differential",
        caption = "@Null_HHockey, data from corsica.ca",
        title = "2017-2018 NHL",
-       subtitle = "Even Strength")
+       subtitle = "Even Strength") +
+  theme(panel.grid.minor = element_blank())
 
 ggsave("running_team_xg/images/team_xg_pm_cum.png")
 
 df %>% 
-  gghighlight_line(aes(game_number, xg_pm_cum, group = team), unique(team) == "MTL") +
+  gghighlight_line(aes(game_number, xg_pm_cum, group = team), unique(team) == "PIT") +
   #scale_x_continuous(expand = c(0,0)) +
   labs(x = "Game Number",
        y = "Cumulative xG Differential")
