eaa9ca327564d0bb871f60266625f082465930f2
Conor Tompkins <conorotompkins@gmail.com>
2017-12-07 22:35:25 -0500

    added code for charts

--- running_team_xg/scripts/running_team_xg.R
+++ running_team_xg/scripts/running_team_xg.R
@@ -4,7 +4,7 @@ library(gghighlight)
 
 theme_set(theme_bw())
 
-data <- read_csv("running_team_xg/data/season_2017_2018.csv")
+data <- read_csv("running_team_xg/data/all_sit_season_2017_2018.csv")
 
 df <- data %>%
   mutate(team = Team,
@@ -18,7 +18,7 @@ df <- data %>%
          xg_pm_cum = cumsum(xg_pm))
 
 df %>% 
-  gghighlight_line(aes(game_number, xg_pm_cum, group = team), last(abs(xg_pm_cum)) > 4) +
+  gghighlight_line(aes(game_number, xg_pm_cum, group = team), last(abs(xg_pm_cum)) > 8) +
   geom_hline(yintercept = 0,
              size = .25,
              linetype = 2) +
@@ -26,16 +26,38 @@ df %>%
                      breaks = 1:max(df$game_number)) +
   labs(x = "Game Number",
        y = "Cumulative xG Differential",
-       caption = "@Null_HHockey, data from corsica.ca",
+       caption = "@Null_HHockey, data from corsica.hockey",
        title = "2017-2018 NHL",
-       subtitle = "Even Strength") +
+       subtitle = "All Situations") +
   theme(panel.grid.minor = element_blank())
-
-ggsave("running_team_xg/images/team_xg_pm_cum.png")
+ggsave("running_team_xg/images/team_xg_pm_cum.png", width = 12, height = 6)
 
 df %>% 
   gghighlight_line(aes(game_number, xg_pm_cum, group = team), unique(team) == "PIT") +
-  #scale_x_continuous(expand = c(0,0)) +
+  geom_hline(yintercept = 0,
+             size = .25,
+             linetype = 2) +
+  scale_x_continuous(expand = c(0.01,0),
+                     breaks = 1:max(df$game_number)) +
   labs(x = "Game Number",
-       y = "Cumulative xG Differential")
+       y = "Cumulative xG Differential",
+       caption = "@Null_HHockey, data from corsica.hockey",
+       title = "2017-2018 NHL",
+       subtitle = "All Situations") +
+  theme(panel.grid.minor = element_blank())
+ggsave("running_team_xg/images/PIT_xg_pm_cum.png", width = 12, height = 6)
 
+df %>% 
+  gghighlight_line(aes(game_number, xg_pm_cum, group = team), unique(team) == "WSH") +
+  geom_hline(yintercept = 0,
+             size = .25,
+             linetype = 2) +
+  scale_x_continuous(expand = c(0.01,0),
+                     breaks = 1:max(df$game_number)) +
+  labs(x = "Game Number",
+       y = "Cumulative xG Differential",
+       caption = "@Null_HHockey, data from corsica.hockey",
+       title = "2017-2018 NHL",
+       subtitle = "All Situations") +
+  theme(panel.grid.minor = element_blank())
+ggsave("running_team_xg/images/VAN_xg_pm_cum.png", width = 12, height = 6)
