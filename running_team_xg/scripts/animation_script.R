8779c877591a2ac420e837de6b0aa10ea03cfbdb
Conor Tompkins <conorotompkins@gmail.com>
2018-01-08 09:26:15 -0500

    created code for animated scatter plots. created gifs

--- running_team_xg/scripts/animation_script.R
+++ running_team_xg/scripts/animation_script.R
@@ -26,3 +26,23 @@ p2 <-  df_smoothed %>%
   facet_wrap(~season, ncol = 1) +
   labs(y = "Expected Goals For")
 gganimate(p2, interval = .1, "xg_animated.gif", ani.width=1000, ani.height=800)
+
+#animated scatter plot
+p3 <- df_smoothed %>% 
+  filter(season == "2017_2018") %>% 
+  ggplot(aes(x = xgf_loess, y = xga_loess, fill = team, label = team, frame = game_number)) +
+  geom_abline() +
+  geom_vline(xintercept = mean(df_smoothed$xgf60)) +
+  geom_hline(yintercept = mean(df_smoothed$xga_loess)) +
+  geom_label(aes(cumulative = FALSE, group = team), show.legend = FALSE) +
+  geom_path(aes(cumulative = TRUE, group = team, color = team)) +
+  coord_equal(xlim = c(1.75, 3), ylim = c(1.75, 3)) +
+  scale_y_reverse() +
+  scale_color_discrete("Team") +
+  labs(x = "Expected Goals For",
+       y = "Expected Goals Against",
+       title = "2017-2018 All Situations",
+       caption = "@Null_HHockey, data from corsica.hockey")
+p3
+#gganimate(p3, interval = .1)
+gganimate(p3, interval = .25, title_frame = FALSE,  "xg_scatter_plot_animated.gif", ani.width=1000, ani.height=1000)
