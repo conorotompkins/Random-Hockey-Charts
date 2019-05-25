f43f7c1b8b8ae04ad232a7db4346e229a19e0f01
Conor Tompkins <conorotompkins@gmail.com>
2019-05-25 00:08:01 +0000

    added EDA charts

--- xg_model/scripts/model.R
+++ xg_model/scripts/model.R
@@ -7,11 +7,24 @@ theme_set(theme_bw())
 df <- read_csv("xg_model/data/Shot Prediction Data for Comparison.csv") %>% 
   clean_names()
 
+df %>% 
+  #filter(str_detect(event_player_1, "CROSBY") | str_detect(event_player_1, "OVECHKIN")) %>% 
+  mutate(coords_x = case_when(coords_x < 0 ~ coords_x * -1,
+                              coords_x >= 0 ~ coords_x)) %>% 
+  ggplot(aes(coords_x, coords_y)) +
+  stat_density_2d(aes(fill = stat(level)), geom = "polygon", alpha = .5) +
+  #facet_wrap(~event_player_1, ncol = 1) +
+  scale_fill_viridis_c()
+
+df %>% 
+  ggplot(aes(coords_x, coords_y)) +
+  geom_point() +
+  facet_wrap(~shot_type)
 
 df %>% 
   ggplot(aes(coords_x, coords_y)) +
   #geom_point()
-  stat_density_2d(aes(fill = goal, geom = "polygon")) +
+  stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
   scale_fill_viridis_c()
 
 df %>% 
@@ -24,13 +37,19 @@ df %>%
   ggplot(aes(coords_y)) +
   geom_histogram()
 
+min(df$coords_x)
+max(df$coords_x)
+min(df$coords_y)
+max(df$coords_y)
+
+
 df_shooting_tile <- df %>% 
   select(coords_x, coords_y, goal) %>% 
-  complete(coords_x, coords_y) %>% 
-  replace_na(list(goal = FALSE)) %>% 
   group_by(coords_x, coords_y) %>% 
   summarize(shoot_percent = mean(goal == TRUE),
-            n = n())
+            n = n()) %>% 
+  complete(coords_x = c(-98:98), coords_y = c(-50:50)) %>% 
+  replace_na(list(shoot_percent = 0, n = 0))
 
 df_shooting_tile %>% 
   ungroup() %>% 
@@ -41,9 +60,17 @@ df_shooting_tile %>%
   geom_point()
 
 df_shooting_tile %>% 
-  ggplot(aes(coords_x, coords_y, fill = shoot_percent)) +
-    geom_density_2d() +
-    scale_fill_viridis_c()
+  ggplot(aes(coords_x, coords_y, fill = shoot_percent, alpha = n)) +
+  geom_tile() +
+  #stat_density_2d(aes(fill = shoot_percent), geom = "polygon") +
+  scale_fill_viridis_c() +
+  theme_void()
+
+df %>% 
+  ggplot(aes(coords_x, coords_y, fill = event_angle)) +
+  geom_tile() +
+  scale_fill_viridis_c()
+
 
 df_model <- df %>% 
   #mutate(goal = as.numeric(goal)) %>% 
@@ -51,17 +78,33 @@ df_model <- df %>%
          coords_x, coords_y, home_skaters, away_skaters, 
          game_score_state, game_strength_state, event_distance, event_angle) %>% 
   mutate_if(is.character, factor) %>% 
-  select(goal, event_distance, event_angle)
+  select(goal, event_distance, event_angle, shot_type)
 
 df_model %>% 
   ggplot(aes(goal)) +
-  geom_histogram()
+  geom_histogram(stat = "count")
 
 df_model %>% 
-  ggplot(aes(event_distance, goal)) +
-  geom_point(alpha = .1) +
-  geom_smooth(method = "glm", method.args = list(family = "binomial"))
+  mutate(goal = as.numeric(goal)) %>% 
+  ggplot(aes(event_distance, goal, color = shot_type, fill = shot_type)) +
+  #geom_point(alpha = .1) +
+  geom_smooth(method = "glm", method.args = list(family = "binomial")) +
+  scale_y_continuous(limits = c(0, 1))
 
+df_model %>% 
+  mutate(goal = as.numeric(goal)) %>% 
+  ggplot(aes(event_angle, goal, color = shot_type, fill = shot_type)) +
+  #geom_point(alpha = .1) +
+  geom_smooth(method = "glm", method.args = list(family = "binomial")) +
+  scale_y_continuous(limits = c(0, 1))
+
+df_model %>% 
+  ggplot(aes(event_distance, event_angle)) +
+  geom_point()
+
+df_aug %>% 
+  select(goal, event_distance, event_angle, .fitted, .predicted, .resid) %>% 
+  View()
 
 model <- glm(goal ~ ., data = df_model, family = "binomial")
 
