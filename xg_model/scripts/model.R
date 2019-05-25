5ce83f3de11932e2b5b07d4116b04bde49ecfc61
Conor Tompkins <conorotompkins@gmail.com>
2019-05-25 19:20:08 +0000

    separated viz from model code

--- xg_model/scripts/model.R
+++ xg_model/scripts/model.R
@@ -7,71 +7,6 @@ theme_set(theme_bw())
 df <- read_csv("xg_model/data/Shot Prediction Data for Comparison.csv") %>% 
   clean_names()
 
-df %>% 
-  #filter(str_detect(event_player_1, "CROSBY") | str_detect(event_player_1, "OVECHKIN")) %>% 
-  mutate(coords_x = case_when(coords_x < 0 ~ coords_x * -1,
-                              coords_x >= 0 ~ coords_x)) %>% 
-  ggplot(aes(coords_x, coords_y)) +
-  stat_density_2d(aes(fill = stat(level)), geom = "polygon", alpha = .5) +
-  #facet_wrap(~event_player_1, ncol = 1) +
-  scale_fill_viridis_c()
-
-df %>% 
-  ggplot(aes(coords_x, coords_y)) +
-  geom_point() +
-  facet_wrap(~shot_type)
-
-df %>% 
-  ggplot(aes(coords_x, coords_y)) +
-  #geom_point()
-  stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
-  scale_fill_viridis_c()
-
-df %>% 
-  ggplot(aes(coords_x)) +
-  geom_histogram()
-
-summary(df$coords_y)
-
-df %>% 
-  ggplot(aes(coords_y)) +
-  geom_histogram()
-
-min(df$coords_x)
-max(df$coords_x)
-min(df$coords_y)
-max(df$coords_y)
-
-
-df_shooting_tile <- df %>% 
-  select(coords_x, coords_y, goal) %>% 
-  group_by(coords_x, coords_y) %>% 
-  summarize(shoot_percent = mean(goal == TRUE),
-            n = n()) %>% 
-  complete(coords_x = c(-98:98), coords_y = c(-50:50)) %>% 
-  replace_na(list(shoot_percent = 0, n = 0))
-
-df_shooting_tile %>% 
-  ungroup() %>% 
-  summarize(rows = sum(n))
-
-df_shooting_tile %>% 
-  ggplot(aes(n, shoot_percent)) +
-  geom_point()
-
-df_shooting_tile %>% 
-  ggplot(aes(coords_x, coords_y, fill = shoot_percent, alpha = n)) +
-  geom_tile() +
-  #stat_density_2d(aes(fill = shoot_percent), geom = "polygon") +
-  scale_fill_viridis_c() +
-  theme_void()
-
-df %>% 
-  ggplot(aes(coords_x, coords_y, fill = event_angle)) +
-  geom_tile() +
-  scale_fill_viridis_c()
-
-
 df_model <- df %>% 
   #mutate(goal = as.numeric(goal)) %>% 
   select(goal, gametime, event_type, shot_type, 
@@ -82,7 +17,7 @@ df_model <- df %>%
 
 df_model %>% 
   ggplot(aes(goal)) +
-  geom_histogram(stat = "count")
+  geom_bar(stat = "count")
 
 df_model %>% 
   mutate(goal = as.numeric(goal)) %>% 
